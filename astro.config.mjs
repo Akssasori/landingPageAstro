9b3766d450db23c6657b2b25a5ec0b2601cf7bac
Lucas Diniz <diniz3003@gmail.com>
2025-07-12 11:16:16 -0300

    ajuste configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://landing-page-astro-9g8m5rg0f-akssasoris-projects.vercel.app',
+});
